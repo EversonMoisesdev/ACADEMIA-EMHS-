21c62d9d5f7a05895e8c9b59a7c3d76a1a5e6a30
EversonMoisesdev <eversonmoises301020@gmail.com>
2023-09-16 17:11:45 -0300

    preenchimento do que foi criado

--- BackEnd/ACADEMIAEMHSAPI/Controllers/AlunoController.cs
+++ BackEnd/ACADEMIAEMHSAPI/Controllers/AlunoController.cs
@@ -1,6 +1,24 @@
-﻿namespace ACADEMIAEMHSAPI.Controllers
+﻿using ACADEMIAEMHSAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ACADEMIAEMHSAPI.Controllers
 {
     public class AlunoController
     {
     }
+}
+[ApiController]
+[Route("[controller]")]
+public class AlunoController : ControllerBase
+{
+    [HttpPost (Name ="CreateAluno")] 
+    public Aluno CreatAluno (Aluno alunoObject)
+    {
+        string retorno = "Aluno criado com sucesso";
+
+        return alunoObject;
+
+      
+    }   
 }
--- BackEnd/ACADEMIAEMHSAPI/Entities/Aluno.cs
+++ BackEnd/ACADEMIAEMHSAPI/Entities/Aluno.cs
@@ -2,5 +2,11 @@
 {
     public class Aluno
     {
+        public int matricula { get; set; }
+        public string? nome { get; set; }
+        public int idade { get; set; }
+        public string? sexo { get; set; }
+        public string altura { get; set; }
+
     }
 }
