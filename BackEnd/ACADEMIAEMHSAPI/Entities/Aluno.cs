28004a095d95c3635f2c56f4b8000c4bc5d50707
EversonMoisesdev <eversonmoises301020>
2023-09-19 09:35:07 -0300

    Adicionei um campo sobrenome

--- BackEnd/ACADEMIAEMHSAPI/Entities/Aluno.cs
+++ BackEnd/ACADEMIAEMHSAPI/Entities/Aluno.cs
@@ -4,6 +4,7 @@
     {
         public int matricula { get; set; }
         public string? nome { get; set; }
+        public string? sobrenome { get; set; }
         public int idade { get; set; }
         public string? sexo { get; set; }
         public string altura { get; set; }
