93fb9aed884340503bfc4388ded75d2b31dbbea9
EversonMoisesdev <eversonmoises301020>
2023-09-23 18:15:20 -0300

    Estude mais isso

--- BackEnd/ACADEMIAEMHSAPI/Controllers/AlunoController.cs
+++ BackEnd/ACADEMIAEMHSAPI/Controllers/AlunoController.cs
@@ -1,6 +1,5 @@
 ﻿using ACADEMIAEMHSAPI.Entities;
 using Microsoft.AspNetCore.Mvc;
-using System.Reflection.Metadata.Ecma335;
 
 namespace ACADEMIAEMHSAPI.Controllers
 {
@@ -8,17 +7,84 @@ namespace ACADEMIAEMHSAPI.Controllers
     {
     }
 }
+
 [ApiController]
 [Route("[controller]")]
 public class AlunoController : ControllerBase
 {
-    [HttpPost (Name ="CreateAluno")] 
-    public Aluno CreatAluno (Aluno alunoObject)
+    //O campo a
+    // CreatAluno é o nome de HttpPost
+    //[HttpPost(Name = "CreateAluno")]
+    //public ActionResult CreatAluno(Aluno alunoObject)
+    ////{
+    //    string retorno = "Aluno criado com sucesso";
+
+    //    if (alunoObject.peso > 150)
+
+    //    {
+    //        string Perigo = " perigo de obesidade ";
+    //        return Ok(Perigo);
+    //    }
+
+    //    else if (alunoObject.peso > 120)
+
+    //    {
+    //        string Cuidado = " Cuidado quase acima do peso ";
+    //        return Ok(Cuidado);
+    //    }
+
+    //   else
+
+    //    {
+    //        string Certo = " Ok ";
+    //        return Ok(Certo);
+    //    }
+
+    //    }
+    //}
+
+    [HttpPost(Name = "MeuPlano")]
+    public ActionResult MeuPlano(Aluno PlanoAluno)
     {
-        string retorno = "Aluno criado com sucesso";
+        //string P = "voce e um aluno que me tras alegria";
+        //string PP = "voce e um aluno que me tras muita alegria";
+        //string PPP = "voce e a alegria em pessoa";
+        string Pagamento1 = "voce e a alegria em pessoa, mas pagar que e bom, nada ne!";
+        string Pagamento2 = "Voce esta de parabens";
 
-        return alunoObject;
+        //if (PlanoAluno.Plano == "P")
+        //{
+        //    return Ok(P);
+        //}
 
-      
-    }   
+        //else if (PlanoAluno.Plano == "PP")
+        //{
+
+        //    return Ok(PP);
+        //}
+
+        //else if (PlanoAluno.Plano == "PPP")
+        //{
+
+        //    return Ok(PPP);
+
+        //}
+
+        if (PlanoAluno.Pagamento == "Pago")
+        {
+
+            return Ok(Pagamento1);
+        }
+        else if (PlanoAluno.Pagamento == "Nao pago")
+        {
+
+            return Ok(Pagamento2);
+        }
+
+        else
+        {
+            return BadRequest();
+        }
+
+    }
 }
--- BackEnd/ACADEMIAEMHSAPI/Entities/Aluno.cs
+++ BackEnd/ACADEMIAEMHSAPI/Entities/Aluno.cs
@@ -2,12 +2,18 @@
 {
     public class Aluno
     {
+        //propriedades ou atributos de aluno
+        public string Plano { get; set; }
         public int matricula { get; set; }
         public string? nome { get; set; }
         public string? sobrenome { get; set; }
         public int idade { get; set; }
         public string? sexo { get; set; }
         public string altura { get; set; }
+        public double peso { get; set; }
+        public string Pagamento { get; set; }
+       
+
 
     }
 }
